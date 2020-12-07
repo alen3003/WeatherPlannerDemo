8c4f2c4fb04f71780d33293a7bbbaef6334f21d0
Alen <sebalj.alen@gmail.com>
2020-12-07 09:32:18 +0100

    Initial Commit

---
+++ WeatherPlannerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherPlannerDemo
+//
+//  Created by Alen Sebalj on 07.12.2020..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
