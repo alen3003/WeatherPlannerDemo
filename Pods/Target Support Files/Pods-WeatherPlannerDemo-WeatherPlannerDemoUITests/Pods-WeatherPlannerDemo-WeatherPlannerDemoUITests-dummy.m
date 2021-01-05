cd009ab65a558383658a4ffc48830cdd956ff55b
Alen <sebalj.alen@gmail.com>
2021-01-05 16:22:30 +0100

    Deleted pods file

--- Pods/Target Support Files/Pods-WeatherPlannerDemo-WeatherPlannerDemoUITests/Pods-WeatherPlannerDemo-WeatherPlannerDemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WeatherPlannerDemo_WeatherPlannerDemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_WeatherPlannerDemo_WeatherPlannerDemoUITests
-@end
