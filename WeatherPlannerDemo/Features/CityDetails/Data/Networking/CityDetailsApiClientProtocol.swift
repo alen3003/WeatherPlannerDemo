d254037f19eab4f0e000fc629272a5a7e6c0ab85
alen3003 <alen.sebalj@five.agency>
2021-09-03 23:50:51 +0200

    Code refactor

--- WeatherPlannerDemo/Features/CityDetails/Data/Networking/CityDetailsApiClientProtocol.swift
+++ WeatherPlannerDemo/Features/CityDetails/Data/Networking/CityDetailsApiClientProtocol.swift
@@ -1,5 +1,7 @@
 import RxSwift
 
 protocol CityDetailsApiClientProtocol {
+
     func fetchPollutionInfo(coordination: City.Coordination) -> Observable<AirPollutionWrapper>
+
 }
--- WeatherPlannerDemo/Features/CityDetails/Data/Repository/CityDetailsRepositoryProtocol.swift
+++ WeatherPlannerDemo/Features/CityDetails/Data/Repository/CityDetailsRepositoryProtocol.swift
@@ -1,5 +1,7 @@
 import RxSwift
 
 protocol CityDetailsRepositoryProtocol {
+
     func fetchPollutionInfo(coordination: City.Coordination, cityID: Int) -> Observable<AirPollution?>
+
 }
